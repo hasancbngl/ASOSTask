14f52752df05033f2e19f32998d2476555c2d901
Adrian Czuczka <adrianczuczka@gmail.com>
2019-07-05 09:56:07 +0100

    Created recipe client

---
+++ app/src/main/java/com/adrianczuczka/asostask/network/RecipeClient.kt
@@ -0,0 +1,22 @@
+package com.adrianczuczka.asostask.network
+
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RecipeClient {
+
+    private const val BASE_URL = "http://mobile.asosservices.com/sampleapifortest/"
+
+    private val retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+    }
+
+    val api: RecipeAPI by lazy {
+        retrofit.create(RecipeAPI::class.java)
+    }
+}
